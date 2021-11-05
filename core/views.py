0d31be88ad5294a88e113c7d9c4604b026039a7f
CynthiaVM <cynthiamouzo@hotmail.com>
2021-11-05 00:43:38 -0300

    app

--- core/views.py
+++ core/views.py
@@ -1,24 +1,22 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "core/index.html")
+    return render(request, 'core/home.html')
 
 def about(request):
-    return render(request, "core/about.html")
+    return render(request, 'core/about.html')
 
 def blog(request):
-    return render(request, "core/blog.html")
+    return render(request, 'core/blog.html')
 
 def contact(request):
-    return render(request, "core/contact.html")
-
-def store(request):
-    return render(request, "core/store.html")
+    return render(request, 'core/contact.html')
 
 def sample(request):
-    return render(request, "core/sample.html")
+    return render(request, 'core/sample.html')
 
 def services(request):
-    return render(request, "core/services.html")
+    return render(request, 'core/services.html')
 
+def store(request):
+    return render(request, 'core/store.html')
